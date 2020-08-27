2181aa6ee7aebe2f23172ebfab7ee7e97d459946
LukeBalfourJCU <69950894+LukeBalfourJCU@users.noreply.github.com>
2020-08-27 17:42:01 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+"""
+CORE PROGRAM
+
+There are 4 options in the main menu. List, Add, Mark Visited, and Quit.
+YOU MUST USE MANUAL ERROR CHECKING
+
+LIST:
+Prints the list of all places based on priority. Unvisited places must be marked with an '*'
+
+ADD:
+Prompts the user to input the place's name, country, and priority. Manual error checking required.
+This data must be put into the memory. It will be saved to the CSV file on exit.
+
+MARK VISITED:
+Print list of places, but the user can choose an unvisited place to be changed to visited.
+If no places are unvisited, print "No unvisited places".
+
+QUIT:
+Save all user data to the CSV file and end the program.
+
+"""
+
+def main():
+
+    name = open("derp.csv", "w")
+
+
+
+
+
+
+
+
+main()
